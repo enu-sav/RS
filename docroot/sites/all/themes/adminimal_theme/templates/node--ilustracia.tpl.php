0bc5db5bf7f6ba1b862e477a1e3db14c01a23c3d
Miroslav Michalicka <miro.michalicka@gmail.com>
2016-03-07 18:32:31 +0100

    Modules, themes, libraries - refactoring needed

---
+++ docroot/sites/all/themes/adminimal_theme/templates/node--ilustracia.tpl.php
@@ -0,0 +1,26 @@
+<?php
+if(!$no_render):
+?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+
+  <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <div class="links"><?php print render($content['links']); ?></div>
+    <?php endif; ?>
+
+    <?php print render($content['comments']); ?>
+  </div>
+
+</div>
+<?php else: ?>
+ 
+<?php endif; ?>
