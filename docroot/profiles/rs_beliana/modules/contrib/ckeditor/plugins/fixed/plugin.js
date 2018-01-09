1a796107e544ee9e3cfc8b5413b2b081e8cabf30
Matej Lehotský <lehotsky@synergiq.sk>
2018-01-09 15:04:10 +0100

    Fixed CKEditor toolbar position (textaera resize)

--- docroot/profiles/rs_beliana/modules/contrib/ckeditor/plugins/fixed/plugin.js
+++ docroot/profiles/rs_beliana/modules/contrib/ckeditor/plugins/fixed/plugin.js
@@ -30,7 +30,7 @@ CKEDITOR.plugins.add('fixed', {
       var inner = document.getElementsByClassName('cke_inner').item(0);
       var scrollvalue = document.documentElement.scrollTop > document.body.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
 
-      toolbar.style.width = content.offsetWidth + "px";
+      toolbar.style.width = "auto";
       toolbar.style.top = "0px";
       toolbar.style.left = "0px";
       toolbar.style.right = "0px";
@@ -40,13 +40,16 @@ CKEDITOR.plugins.add('fixed', {
       if (toolbar.offsetTop + calculatedOffset <= scrollvalue) {
         toolbar.style.position = "fixed";
         toolbar.style.top = admin.offsetHeight + "px";
+        toolbar.style.width = content.offsetWidth + "px";
         content.style.paddingTop = toolbar.offsetHeight + "px";
       }
 
       if (editor.offsetTop + calculatedOffset > scrollvalue && (editor.offsetTop + editor.offsetHeight) >= (scrollvalue + toolbar.offsetHeight)) {
         toolbar.style.top = "0px";
+        toolbar.style.width = content.offsetWidth + "px";
         toolbar.style.position = "relative";
         content.style.paddingTop = "0px";
+        
       }
 
       if ((editor.offsetTop + editor.offsetHeight) + calculatedOffset < (scrollvalue + toolbar.offsetHeight)) {
