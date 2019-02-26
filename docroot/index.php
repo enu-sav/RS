a7d3fd7b5d539d82b22769551c78f7ec3f0b1d0e
Matej Lehotský <lehotsky@synergiq.sk>
2019-02-26 11:47:36 +0100

    Update logger (ENCB-32)

--- docroot/index.php
+++ docroot/index.php
@@ -18,4 +18,21 @@ define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+// log $_POST and $_GET
+if (substr($_GET['q'], 0, 4) == 'node') {
+  global $user;
+  
+  $log_folder = 'public://logs/' . date('Ym');
+  file_prepare_directory($log_folder, FILE_CREATE_DIRECTORY);
+
+  $filename = drupal_realpath($log_folder . '/log-' . date('d') . '.txt');
+  $log_prefix = date('Y-m-d H:i:s') . ' [' . $user->name . '] - ';
+
+  error_log($log_prefix . "GET: \n" . serialize($_GET) . "\n", 3, $filename);
+  if (!empty($_POST)) {
+    error_log($log_prefix . "POST (" . $_GET['q'] . "): \n" . serialize($_POST) . "\n\n", 3, $filename);
+  }
+}
+
 menu_execute_active_handler();
