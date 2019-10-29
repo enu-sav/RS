eabb8090a03cff28804486989a23026b7d90424d
Matej Lehotský <lehotsky@improve.sk>
2019-10-29 14:32:55 +0100

    Update ckeditor offset (ENCB-190)

--- docroot/profiles/rs_beliana/modules/contrib/ckeditor/plugins/fixed/plugin.js
+++ docroot/profiles/rs_beliana/modules/contrib/ckeditor/plugins/fixed/plugin.js
@@ -22,12 +22,12 @@ CKEDITOR.plugins.add('fixed', {
     }, false);
 
     function calculatePosition() {
-      var calculatedOffset = 95;
       var content = document.getElementsByClassName('cke_contents').item(0);
       var toolbar = document.getElementsByClassName('cke_top').item(0);
       var admin = document.getElementById('admin-menu');
       var editor = document.getElementsByClassName('cke').item(0);
       var inner = document.getElementsByClassName('cke_inner').item(0);
+      var calculatedOffset = 85 + admin.offsetHeight;
       var scrollvalue = document.documentElement.scrollTop > document.body.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
 
       toolbar.style.width = "auto";
@@ -37,14 +37,14 @@ CKEDITOR.plugins.add('fixed', {
       toolbar.style.margin = "0 auto";
       toolbar.style.boxSizing = "border-box";
 
-      if (toolbar.offsetTop + calculatedOffset <= scrollvalue) {
+      if (toolbar.offsetTop + 38 <= scrollvalue) {
         toolbar.style.position = "fixed";
-        toolbar.style.top = admin.offsetHeight + "px";
+        toolbar.style.top = calculatedOffset + "px";
         toolbar.style.width = content.offsetWidth + "px";
         content.style.paddingTop = toolbar.offsetHeight + "px";
       }
 
-      if (editor.offsetTop + calculatedOffset > scrollvalue && (editor.offsetTop + editor.offsetHeight) >= (scrollvalue + toolbar.offsetHeight)) {
+      if (editor.offsetTop + 38 > scrollvalue && (editor.offsetTop + editor.offsetHeight) >= (scrollvalue + toolbar.offsetHeight)) {
         toolbar.style.top = "0px";
         toolbar.style.width = content.offsetWidth + "px";
         toolbar.style.position = "relative";
