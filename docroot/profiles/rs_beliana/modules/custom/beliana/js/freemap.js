e012e66b122dedfebea516c97b55dd8af7f3682b
petersykora <sykora@improve.sk>
2022-07-13 14:24:15 +0200

    BEL-65 - ilustracia - added geodata images creator.

--- docroot/profiles/rs_beliana/modules/custom/beliana/js/freemap.js
+++ docroot/profiles/rs_beliana/modules/custom/beliana/js/freemap.js
@@ -6,10 +6,11 @@
 (function ($, Drupal) {
   Drupal.behaviors.belianaFreeMap = {
     attach: function (context, settings) {
+      var self = this;
       var body = $(document.body);
       var freemap_url = $('#edit-field-freemap-url input');
       var freemap_url_diela = $('#field-url-diela-l-add-more-wrapper input');
-      var freemap_img_fid = $('#edit-field-mapa-obrazok-fid input');
+      var map_img_fid = $('#edit-field-mapa-obrazok-fid input');
 
       window.addEventListener("message", (e) => {
         try {
@@ -25,18 +26,18 @@
         // console.log("URL", e.data.freemap.payload);
       });
 
-      body.find('.freemap-wrapper').click(function () {
+      body.find('.freemap-wrapper').one("click", function () {
         $('.freemap-wrapper #freemap').css('pointer-events', 'auto');
       });
 
-      body.find('.freemap-wrapper').mouseleave(function() {
+      body.find('.freemap-wrapper').mouseleave(function () {
         $('.freemap-wrapper #freemap').css('pointer-events', 'none');
       });
 
-      body.find('#free-map-btn').click(function () {
+      body.find('#free-map-btn').unbind('click').click(function () {
         var title = $('#title-field-add-more-wrapper input').val();
 
-        if (title.length === 0){
+        if (title.length === 0) {
           alert('Pole Názov ilustrácie nieje vyplnené.');
           return;
         }
@@ -44,17 +45,18 @@
         $.ajax({
           url: location.protocol + "//" + location.host + "/get-freemap-image",
           data: {
-            "freemap_title": "mapa-" + title,
+            "title": "mapa-" + title,
             "freemap_url": freemap_url_diela.val(),
-            "freemap_img_fid": freemap_img_fid.val(),
+            "map_img_fid": map_img_fid.val(),
           },
           success: function (data, status, xhr) {
             if (data['status']) {
               $('.image-preview').css('display', 'block');
-              freemap_img_fid.val(data['file_fid']);
-              $('#free-map-img').attr("src", data['file_url'] + `?v=${new Date().getTime()}`);
+              map_img_fid.val(data['file_fid_jpg']);
+              $('#free-map-img').attr("src", data['file_url_jpg'] + `?v=${new Date().getTime()}`);
               alert('Obrázok mapy bol vytvorený');
-            } else {
+            }
+            else {
               alert('Obrázok nebol vytvorený. Kontaktujte administrátora');
             }
           },
@@ -64,27 +66,45 @@
         });
       });
 
-      body.find('#free-map-btn-place').click(function () {
+      body.find('#geodata-btn-place').unbind('click').click(function () {
         var title = $("#title-field-add-more-wrapper input").val();
+        var comment_instruction = $('#field-komentar-instrukcie-add-more-wrapper textarea');
+        var geodata_country = $('select.geodata-country-wrapper');
+        var geodata_region = $('select.geodata-region-wrapper');
+        var geodata_district = $('select.geodata-district-wrapper');
+        var geodata_municipality = $('select.geodata-municipality-wrapper');
 
-        if (title.length === 0){
+        if (title.length === 0) {
           alert('Pole Názov ilustrácie nieje vyplnené.');
+          return;
+        }
+
+        if (geodata_country.val() === "0") {
+          alert('Pole Krajina nieje vybratá.');
+          return;
         }
 
         $.ajax({
-          url: location.protocol + "//" + location.host + "/get-freemap-image",
+          url: location.protocol + "//" + location.host + "/get-geodata-image",
           data: {
-            "freemap_title": "mapa-poloha-" + title,
-            "freemap_url": freemap_url_diela.val(),
-            "freemap_img_fid": freemap_img_fid.val(),
+            "title": "mapa-poloha-" + title,
+            "map_img_fid": map_img_fid.val(),
+            "geodata_country": geodata_country.val(),
+            "geodata_region": geodata_region.val(),
+            "geodata_district": geodata_district.val(),
+            "geodata_municipality": geodata_municipality.val(),
           },
           success: function (data, status, xhr) {
             if (data['status']) {
-              $('.image-preview-place').css('display', 'block');
-              freemap_img_fid.val(data['file_fid']);
-              $('#free-map-img-place').attr("src", data['file_url'] + `?v=${new Date().getTime()}`);
-              alert('Obrázok mapy bol vytvorený');
-            } else {
+              $('.image-preview').css('display', 'block');
+              map_img_fid.val(data['file_fid_jpg'] + '/' + data['file_fid_svg']);
+              $('#geodata-img').attr("src", data['file_url_jpg'] + `?v=${new Date().getTime()}`);
+
+              self.commentInstructionAddValue(comment_instruction);
+
+              alert('Obrázok bol vytvorený');
+            }
+            else {
               alert('Obrázok nebol vytvorený. Kontaktujte administrátora');
             }
           },
@@ -93,6 +113,29 @@
           }
         });
       });
+    },
+    commentInstructionAddValue: function (comment_instruction) {
+      if (comment_instruction.val().length !== 0) {
+        comment_instruction.val(comment_instruction.val() + "\n");
+      }
+
+      var country = $('select.geodata-country-wrapper option:selected').text();
+      var region = $('select.geodata-region-wrapper option:selected').text();
+      var district = $('select.geodata-district-wrapper option:selected').text();
+      var municipality = $('select.geodata-municipality-wrapper option:selected').text();
+
+      if (country !== '- Zvoľiť -') {
+        comment_instruction.val(comment_instruction.val() + 'Generovanie mapy poloha: ' + country);
+      }
+      if (region !== '- Zvoľiť -') {
+        comment_instruction.val(comment_instruction.val() + ' / ' + region);
+      }
+      if (district !== '- Zvoľiť -') {
+        comment_instruction.val(comment_instruction.val() + ' / ' + district);
+      }
+      if (municipality !== '- Zvoľiť -') {
+        comment_instruction.val(comment_instruction.val() + ' / ' + municipality);
+      }
     },
   };
 
