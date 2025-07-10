ccef753292344b7d9141a61f35547fc0830ae4a4
milossramek <msramek22@gmail.com>
2025-07-10 11:46:25 +0000

    User must himself add new comment in BKB when adding it CKEditor

--- docroot/profiles/rs_beliana/modules/custom/ckeditor_bkb_comment/ckeditor_plugin/plugin.js
+++ docroot/profiles/rs_beliana/modules/custom/ckeditor_bkb_comment/ckeditor_plugin/plugin.js
@@ -81,9 +81,9 @@ CKEDITOR.plugins.add('ckeditor_bkb_comment', {
                     if (btn.innerText.includes('Vytvoriť')) {
                       if (success) {
                         let commentId = data[0].parent;
-                        btn.innerText = Drupal.t('Vytvoriť ďalší Komentár k heslu v BKB');
+                        btn.innerText = Drupal.t('Vytvoriť ďalší komentár k heslu v BKB');
                         btn.onclick = function () {
-                          window.open(Drupal.settings.ckeditor_bkb_comment.bkb_edit_url + commentId + '/edit?operation=add-new-comment', '_blank');
+                          window.open(Drupal.settings.ckeditor_bkb_comment.bkb_edit_url + commentId + '/edit', '_blank');
                           const dialog = CKEDITOR.dialog.getCurrent();
                           if (dialog) dialog.hide();
                         };
